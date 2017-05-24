d6d870dbc96956cb26532d873e1e077558d5c953
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-05-25 00:46:16 +0200

    app working localy

---
+++ app.js
@@ -0,0 +1,43 @@
+var months = [ "january", "february", "march", "april", "may",
+ "june", "july", "august", "september", "october",
+  "november", "december" ];
+
+const express = require('express');
+
+const app = express();
+
+app.get('*', function (req, res) {
+	let url = req.url.split('%20');
+	var date;
+	var unix;
+	var natural;
+
+	if (url.length === 3
+		&& months.indexOf(url[0].substring(1).toLowerCase()) != -1
+		&& parseInt(url[1]) <= 31
+		&& url[2].length === 4) {
+		// URL represents a natural language date
+		let year = parseInt(url[2]);
+		let month = months.indexOf(url[0].substring(1).toLowerCase());
+		let day = parseInt(url[1]);
+		date = new Date(year, month, day);
+		console.log('day: ' + day);
+	} else if (url.length === 1
+		&& !isNaN(url[0].substring(1))) {
+		// URL represents a unix time
+		date = new Date(parseInt(url[0].substring(1)) * 1000);
+	}
+	if (date) {
+		unix = date.getTime() / 1000;
+		natural = months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+	} else {
+		unix = null;
+		natural = null;
+	}
+	let result = { 'unix': unix, 'natural': natural };
+  	res.send(result);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
